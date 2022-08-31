2e8667bf67e1255cfb27522460e585b7c8524a08
XZX-hiakuyo <Hiakuyo5786@Gmail.com>
2022-08-31 16:42:03 +0800

    hiakuyo: migrate the frontend into the project *the login function worked at the login_sample.html

--- src/main/resources/backend/js/LogInApi.js
+++ src/main/resources/backend/js/LogInApi.js
@@ -3,7 +3,7 @@ const LoginApp = Vue.createApp({
         return{
             LoginForm:{
                 username:"root",
-                password:"root"
+                password:null
             },
             info:null
         }
@@ -15,10 +15,15 @@ const LoginApp = Vue.createApp({
                 .post('/user/login',that.LoginForm)
                 .then(function (res){
                     if(res.data.code === 1){
-                        console.log("login accepted,code:" + res.data.code);
+                        console.log("login accepted,code: " + res.data.code);
+                        console.log("userid: " + localStorage.getItem("user"));
+                        console.log(res);
+                        // window.location.replace("/backend/test.html");
                         window.location.replace("/backend/main.html");
                     }else{
                         console.log("login failed,code:" + res.data.code);
+                        window.alert("登陆失败！");
+                        window.location.replace("/backend/login_sample.html");
                     }
                 })
         }
