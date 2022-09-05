039170322e21c68cc3b8c3b2ffac99eb779e55bc
zhongziwen <zhongziwen@ziwen.com>
2022-09-05 15:13:46 +0800

    激活信息mapper接口

---
+++ src/main/java/com/example/mapper/activation_informationMapper.java
@@ -0,0 +1,7 @@
+package com.example.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.entity.activation_information;
+
+public interface activation_informationMapper extends BaseMapper<activation_information> {
+}
