9e11b77c4f1eb303fa410f61cbbf74541c6ed90e
zhongziwen <zhongziwen@ziwen.com>
2022-09-06 15:20:26 +0800

    激活信息接口

--- src/main/java/com/example/controller/ApplynameController.java
+++ src/main/java/com/example/controller/ApplynameController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.example.common.R;
 import com.example.entity.Applyname;
 import com.example.service.ApplynameService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
@@ -23,11 +23,29 @@ public class ApplynameController {
      * @param applyname
      * @return
      */
+    /*
     @GetMapping("/list")
     public List<Applyname> list(@RequestBody Applyname applyname){
+
         LambdaQueryWrapper<Applyname> queryWrapper = new LambdaQueryWrapper();
         queryWrapper.eq(Applyname::getApplyname,applyname);
-
         return applynameService.list(queryWrapper);
+    }
+     */
+
+    @GetMapping("/list1")
+    public R<List> list(Applyname applyname){
+        LambdaQueryWrapper<Applyname> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Applyname::getApplyname,applyname.getApplyname());
+        List<Applyname> list = applynameService.list(queryWrapper);
+        return R.success(list);
+    }
+
+    @GetMapping("/list2")
+    public R<List> list(String applyname){
+        LambdaQueryWrapper<Applyname> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Applyname::getApplyname,applyname);
+        List<Applyname> list = applynameService.list(queryWrapper);
+        return R.success(list);
     }
 }
